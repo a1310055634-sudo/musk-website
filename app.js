7a9e28e5868f80b68292cdbf6984f93efbcb9446
Musk Inc. AutoBuilder <autobuilder@local>
2026-09-13 04:13:16 +0800

    v0.9.0 M08 商业方法论板块：六张方法卡(原则+案例+适用边界) · 含2项自主优化

--- app.js
+++ app.js
@@ -7,7 +7,7 @@
 'use strict';
 
 (function () {
-  var SITE_VERSION = '0.8.0';
+  var SITE_VERSION = '0.9.0';
 
   /* ---------- 版本号（页脚与报头共用 .site-version-val） ---------- */
   document.querySelectorAll('.site-version-val').forEach(function (el) {
