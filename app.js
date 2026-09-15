a85c4bce38ad53b71038c9ee96c933b0b55b3647
Musk Inc. AutoBuilder <autobuilder@local>
2026-09-15 08:10:03 +0800

    v3.7.1 补记：切除四子页残留损坏导航并以干净导航重建（上轮失败脚本的半成品残留）

--- app.js
+++ app.js
@@ -7,7 +7,7 @@
 'use strict';
 
 (function () {
-  var SITE_VERSION = '4.6.0';
+  var SITE_VERSION = '4.7.0';
 
   /* ---------- 版本号（页脚与报头共用 .site-version-val） ---------- */
   document.querySelectorAll('.site-version-val').forEach(function (el) {
