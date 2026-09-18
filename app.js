880a0290c29524cbff272730369c8c996bdf4221
Musk Inc. AutoBuilder <autobuilder@local>
2026-09-19 05:40:29 +0800

    v5.60.0 Phase3-2第二批: finance.html补齐X与xAI财务部(收入曲线/Fidelity减记/xAI五轮融资序列, WebSearch核实), 财务全景四部齐备

--- app.js
+++ app.js
@@ -7,7 +7,7 @@
 'use strict';
 
 (function () {
-  var SITE_VERSION = '5.59.0';
+  var SITE_VERSION = '5.60.0';
 
   /* ---------- 版本号（页脚与报头共用 .site-version-val） ---------- */
   document.querySelectorAll('.site-version-val').forEach(function (el) {
